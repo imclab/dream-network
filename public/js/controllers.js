c606f14e0bb7f574f8ae5bcd80ec4e02d9ace0cb
Rahul Kapadia <rahul@ringrevenue.com>
2013-11-21 07:11:05 -0800

    added cloud for hobbies

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -100,7 +100,52 @@ function SearchCtrl($scope, $routeParams) {
 }
 
 function InformationCtrl($scope, $routeParams) {
+    var interests = [];
+    var query = new Parse.Query(Parse.User);
+    query.select("interests");
+    query.each(function(result) {
+        interests = interests.concat(result.attributes.interests.split(','))
+    }).then(function() {
+            var fill = d3.scale.category20();
+            d3.layout.cloud().size([850, 300])
+                .words(interests.map(function(d) {
+                        return {text: d, size: 1 + Math.random() * 90};
+                    }))
+                .padding(3)
+                .rotate(function() { return ~~(Math.random() * 2) * 90; })
+                .font("Impact")
+                .fontSize(function(d) { return d.size; })
+                .on("end", draw)
+                .start();
+
+            function draw(words) {
+                console.log(interests)
+                d3.select("div#hobbies").append("svg")
+                    .attr("width", 850)
+                    .attr("height", 300)
+                    .append("g")
+                    .attr("transform", "translate(150,150)")
+                    .selectAll("text")
+                    .data(words)
+                    .enter().append("text")
+                    .style("font-size", function(d) { return d.size + "px"; })
+                    .style("font-family", "Impact")
+                    .style("fill", function(d, i) { return fill(i); })
+                    .attr("text-anchor", "middle")
+                    .attr("transform", function(d) {
+                        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                    })
+                    .text(function(d) { return d.text; });
+            }
+        });
+
+
+
 }
+
+
+
+
 
 function AboutCtrl($scope, $routeParams) {
 }
